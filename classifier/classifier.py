54e07ccf2fbf955f47edb2181e54d0a67c794423
Sandrro <antonov-as@yandex.ru>
2023-05-05 21:31:10 +0300

    changed script to class, removed outdated notebooks

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,36 +1,21 @@
-import pandas as pd
-from tqdm import tqdm
 from transformers import pipeline
-
+from tqdm import tqdm
+import pandas as pd
 tqdm.pandas()
 
+class TextClassifier:
+    def __init__(self, repository_id:str = "Sandrro/cc-model", number_of_categories:int = 1, device_type = None):
+          self.REP_ID = repository_id
+          self.CATS_NUM = number_of_categories
+          self.classifier = pipeline("text-classification", model=self.REP_ID, tokenizer='cointegrated/rubert-tiny2', max_length=2048, truncation=True, device=device_type)
 
-def category_predictor(text, classifier):
-    pred = pd.DataFrame(classifier(text, top_k=3))
-    classifier.call_count = 0
-    cats = ", ".join(pred["label"].tolist())
-    probs = ", ".join(pred["score"].round(3).astype(str).tolist())
-    return [cats, probs]
-
-
-def classify_text(path_to_excel, model_name="Sandrro/cc-model", device=0):
-    classifier = pipeline(
-        "text-classification",
-        model=model_name,
-        tokenizer=model_name,
-        max_length=2048,
-        truncation=True,
-        device=device,
-    )
-    df_predict = pd.read_excel(path_to_excel)
-    df_predict[["cats", "probs"]] = pd.DataFrame(
-        df_predict["Текст"]
-        .progress_map(lambda x: category_predictor(x, classifier))
-        .to_list()
-    )
-    df_predict["category"] = df_predict["cats"].map(lambda x: x.split(", ")[0])
-    df_predict["probability"] = df_predict["probs"].map(
-        lambda x: float(x.split(", ")[0])
-    )
-
-    return df_predict
+    def run(self, t)-> list[pd.Series]:
+        pred = pd.DataFrame(self.classifier(t, top_k=self.CATS_NUM))
+        self.classifier.call_count = 0 #else warnings
+        if self.CATS_NUM > 1:
+            cats = ', '.join(pred['label'].tolist())
+            probs = ', '.join(pred['score'].round(3).astype(str).tolist())
+        else:
+            cats = pred['label'][0]
+            probs = pred['score'].round(3).astype(str)[0]
+        return [cats, probs]
